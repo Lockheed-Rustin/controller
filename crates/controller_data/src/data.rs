5837e0ec3bf53621f3fa3c583198f91df731bb53
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2024-12-04 11:32:19 +0100

    new stats

--- crates/controller_data/src/data.rs
+++ crates/controller_data/src/data.rs
@@ -3,26 +3,38 @@ use std::collections::HashMap;
 use eframe::egui::Context;
 
 use wg_2024::network::NodeId;
-
+use wg_2024::packet::{Packet, PacketType};
 
 pub struct SimulationData {
-    pub logs : HashMap<NodeId, Vec<String>>,
+    pub logs: HashMap<NodeId, Vec<String>>,
     pub stats: HashMap<NodeId, DroneStats>,
     pub ctx: Context,
 }
 
 impl SimulationData {
-    pub fn new(logs: HashMap<NodeId, Vec<String>>, stats: HashMap<NodeId, DroneStats>, ctx: Context) -> Self {
-        Self {
-            logs,
-            stats,
-            ctx
-        }
+    pub fn new(
+        logs: HashMap<NodeId, Vec<String>>,
+        stats: HashMap<NodeId, DroneStats>,
+        ctx: Context,
+    ) -> Self {
+        Self { logs, stats, ctx }
     }
 }
 
 #[derive(Default)]
 pub struct DroneStats {
-    forwarded_packets: u32,
-    dropped_packets: u32,
+    packets_forwarded: HashMap<PacketType, u32>,
+    packets_dropped: HashMap<PacketType, u32>,
+}
+
+#[derive(Default)]
+pub struct ClientStats {
+    packets_sent: HashMap<PacketType, u32>,
+    packets_received: HashMap<PacketType, u32>,
+}
+
+#[derive(Default)]
+pub struct ServerStats {
+    packets_sent: HashMap<PacketType, u32>,
+    packets_received: HashMap<PacketType, u32>,
 }
