c15eed0b471d146f60c9e6c224c1dfcd752ee6b0
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2025-02-08 17:48:16 +0100

    clippy pedant 2

--- src/ui_components/drone_window.rs
+++ src/ui_components/drone_window.rs
@@ -37,9 +37,7 @@ pub fn spawn_drone_window(
                     .width(50.0)
                     .selected_text(
                         state
-                            .add_link_selected_id
-                            .map(|num| num.to_string())
-                            .unwrap_or_else(|| "-".to_string()),
+                            .add_link_selected_id.map_or_else(|| "-".to_string(), |num| num.to_string()),
                     )
                     .show_ui(ui, |ui| {
                         for number in node_ids {
@@ -59,12 +57,12 @@ pub fn spawn_drone_window(
                                     // push log to other node as well
                                     mutex.add_log(
                                         sid,
-                                        format!("Link added with node {}", id),
+                                        format!("Link added with node {id}"),
                                         Color32::WHITE,
                                     );
-                                    format!("Link added with node {}", sid)
+                                    format!("Link added with node {sid}")
                                 }
-                                None => format!("Failed to add link with node {}", sid),
+                                None => format!("Failed to add link with node {sid}"),
                             }
                         }
                     };
